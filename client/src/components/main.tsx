2d40b56fb0b12d874df61339048b711dcfc2ec8f
Jennifer Tendell <jennifer.tendell@gmail.com>
2021-05-06 15:17:57 +0200

    Fix layout for sidebar.

--- client/src/components/main.tsx
+++ client/src/components/main.tsx
@@ -1,15 +1,10 @@
 import { CSSProperties } from "react";
 import ChatContainer from "./ChatContainer";
 import Sidebar from "./Sidebar";
-import { useContext } from "react";
-import { SocketContext } from "../contexts/socketProvider";
 
 function Main() {
-    const { username } = useContext(SocketContext);
-
     return(
         <main style={rootStyle}>
-           <p>{username}</p>
            <Sidebar />
            <ChatContainer />
         </main>
