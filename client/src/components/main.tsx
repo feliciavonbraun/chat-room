a0c0c17234a9e241177dc69813c05c9d2c3c439b
Jennifer Tendell <jennifer.tendell@gmail.com>
2021-05-07 11:41:08 +0200

    Add form for 'New Chat'.

--- client/src/components/main.tsx
+++ client/src/components/main.tsx
@@ -1,12 +1,24 @@
-import { CSSProperties } from "react";
+import { CSSProperties, useState } from "react";
 import ChatContainer from "./ChatContainer";
+import NewChatForm from "./NewChatForm";
 import Sidebar from "./Sidebar";
 
 function Main() {
+    const [showForm, setShowForm] = useState(false)
+
+    function toggleNewChatForm() {
+        setShowForm(!showForm);
+    };
+    
     return(
         <main style={rootStyle}>
-           <Sidebar />
-           <ChatContainer />
+           <Sidebar 
+                openForm={toggleNewChatForm} 
+            />
+           {showForm 
+            ?   <NewChatForm closeForm={toggleNewChatForm}/>
+            :   <ChatContainer />
+           }
         </main>
     );
 };
